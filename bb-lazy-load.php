5c75234c1eba8a2e398b7f687f3ad7a8ad124dd2
Clay Harmon <clayharmon@Clays-MacBook-Pro.local>
2019-02-21 18:47:24 -0500

    Add a WordPress settings page.

--- bb-lazy-load.php
+++ bb-lazy-load.php
@@ -3,36 +3,142 @@
 Plugin Name: Beaver Builder - Lazy Load
 Description: Lazy loads background images set using Beaver Builder. Also will serve .webp if setting is selected.
 Author: Clay Harmon
-Version: 0.1
+Version: 0.2
 */
-function bbll_load_module_examples() {
+
+function bbll_load_module_add_filters() {
   if ( class_exists( 'FLBuilder' ) ) {
-    add_filter( 'fl_builder_row_attributes', 'bbll_builder_render_attrs', 10, 2 );
-    add_filter( 'fl_builder_column_attributes', 'bbll_builder_render_attrs', 10, 2 );
-    //add_filter( 'fl_builder_render_module_content', 'bbll_builder_render_module_html', 10, 2);
+    add_filter( 'fl_builder_row_attributes', 'bbll_builder_render_attrs_row', 10, 2 );
+    add_filter( 'fl_builder_column_attributes', 'bbll_builder_render_attrs_col', 10, 2 );
+
+    $bbll_options = get_option('bbll_store');
+    if($bbll_options['image_html']){
+      add_filter( 'fl_builder_render_module_content', 'bbll_builder_render_module_html', 10, 2);
+    }
+
     add_filter( 'fl_builder_render_css', 'bbll_builder_render_css', 10, 3 );
     add_action( 'wp_footer', 'bbll_lazyload_bgs' );
     add_action( 'wp_head', 'bbll_custom_styles' );
   }
 }
-add_action( 'init', 'bbll_load_module_examples' );
+add_action( 'init', 'bbll_load_module_add_filters' );
 
-function bbll_builder_render_attrs( $attrs, $container ) {
+add_action( 'admin_action_bbll_admin_form', 'bbll_admin_form_action' );
+function bbll_admin_form_action(){
+    //var_dump($_POST);
+    $bbll_options = [];
+
+    if( isset( $_POST['bbll_option_webp'] ) ){
+      $value = esc_sql( $_POST['bbll_option_webp'] );
+      $bbll_options['webp'] = $value;
+    }
+
+    if( isset( $_POST['bbll_option_column_images'] ) ){
+      $value = esc_sql( $_POST['bbll_option_column_images'] );
+      $bbll_options['column_images'] = $value;
+    }
+
+    if( isset( $_POST['bbll_option_row_images'] ) ){
+      $value = esc_sql( $_POST['bbll_option_row_images'] );
+      $bbll_options['row_images'] = $value;
+    }
+
+    if( isset( $_POST['bbll_option_row_parallax'] ) ){
+      $value = esc_sql( $_POST['bbll_option_row_parallax'] );
+      $bbll_options['row_parallax'] = $value;
+    }
+
+    if( isset( $_POST['bbll_option_image_html'] ) ){
+      $value = esc_sql( $_POST['bbll_option_image_html'] );
+      $bbll_options['image_html'] = $value;
+    }
+
+    update_option('bbll_store', $bbll_options);
+    FLBuilderModel::delete_asset_cache_for_all_posts();
+    wp_redirect( $_SERVER['HTTP_REFERER'] );
+    exit();
+}
+
+function bbll_add_settings_page(){
+  add_options_page(
+    'BB Lazy Load',
+    'BB Lazy Load',
+    'manage_options',
+    'bbll',
+    'bbll_settings_html'
+  );
+}
+add_action( 'admin_menu', 'bbll_add_settings_page' );
+function bbll_checkbox_html($options_array, $option_name, $label){
+  $bbll_options = $options_array;
+  $ischecked = ($bbll_options[$option_name]) ? 'checked' : '';
+
+  $output = '';
+  $output .= '<div style="padding:5px 0;">';
+  $output .= '<label for="bbll_option_'.$option_name.'"><input name="bbll_option_'.$option_name.'" type="checkbox" id="bbll_option_'.$option_name.'" value="1" '.$ischecked.' >'.$label.'</label>';
+  $output .= '</div>';
+  return $output;
+}
+function bbll_settings_html(){
+  $bbll_options = get_option('bbll_store');
+  //var_dump($bbll_options);
+  ?>
+  <div class="wrap">
+    <h1>Beaver Builder Lazy Load</h1>
+    <form style="padding:10px 0;" method="POST" action="<?php echo admin_url( 'admin.php' ); ?>">
+      <?php echo bbll_checkbox_html($bbll_options, 'webp', 'Enable .webp? <em>exampleimage.jpg.webp</em>'); ?>
+
+      <?php echo bbll_checkbox_html($bbll_options, 'column_images', 'Enable lazy loading for column background images?'); ?>
+
+      <?php echo bbll_checkbox_html($bbll_options, 'row_images', 'Enable lazy loading for row background images?'); ?>
+
+      <?php echo bbll_checkbox_html($bbll_options, 'row_parallax', 'Enable lazy loading for row parallax images?'); ?>
+
+      <?php echo bbll_checkbox_html($bbll_options, 'image_html', 'Enable lazy load for all Beaver Builder img tags? <strong>(Experimental)</strong>'); ?>
+      
+      <input type="hidden" name="action" value="bbll_admin_form" />
+      <input style="margin:10px 0;" type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes" />
+    </form>
+  </div>
+  <?php
+}
+
+function bbll_builder_render_attrs_row( $attrs, $container ) {
   if(!isset($container->settings->bg_image_src)) return $attrs;
 
+  $bbll_options = get_option('bbll_store');
   $image = $container->settings->bg_image_src;
 
   if ((isset($_SERVER['HTTP_ACCEPT']) === true) && (strstr($_SERVER['HTTP_ACCEPT'], 'image/webp') !== false)) {
-    // we'll need to activate this via a setting.
-    // $image .= '.webp';
+    if($bbll_options['webp']){
+      $image .= '.webp';
+    }
   }
 
-  if($container->settings->bg_type === 'photo'){
+  if($container->settings->bg_type === 'photo' && $bbll_options['row_images']){
     $attrs['data-bb-lazy-load-bgurl'] = $image;
   }
 
-  if($container->settings->bg_type === 'parallax'){
+  if($container->settings->bg_type === 'parallax' && $bbll_options['row_parallax']){
     $attrs['data-parallax-image'] = '';
+    $attrs['data-bb-lazy-load-bgurl'] = $image;
+  }
+  return $attrs;
+}
+
+function bbll_builder_render_attrs_col( $attrs, $container ) {
+  if(!isset($container->settings->bg_image_src)) return $attrs;
+
+  $bbll_options = get_option('bbll_store');
+  $image = $container->settings->bg_image_src;
+
+  if ((isset($_SERVER['HTTP_ACCEPT']) === true) && (strstr($_SERVER['HTTP_ACCEPT'], 'image/webp') !== false)) {
+    if($bbll_options['webp']){
+      $image .= '.webp';
+    }
+  }
+
+  if($container->settings->bg_type === 'photo' && $bbll_options['column_images']){
     $attrs['data-bb-lazy-load-bgurl'] = $image;
   }
   return $attrs;
@@ -59,7 +165,13 @@ function bbll_builder_render_module_html($content, $module) {
 
 function bbll_builder_render_css( $css, $nodes, $global_settings ) {
   $matches = array();
-  if (preg_match_all('/\.fl-node-(.*?)\ >\ (?:.fl-row-content-wrap|.fl-col-content)\ {[ \n]?[ \t]?background-image:[ ]?url\([\'"]?(.*?)\)/', $css, $matches)) {
+  $bbll_options = get_option('bbll_store');
+  if ($bbll_options['row_images'] && preg_match_all('/\.fl-node-(.*?)\ >\ (?:.fl-row-content-wrap)\ {[ \n]?[ \t]?background-image:[ ]?url\([\'"]?(.*?)\)/', $css, $matches)) {
+    for($i=0;$i<count($matches[2]);$i++){
+      $css = str_replace("\n\tbackground-image: url(".$matches[2][$i].");\n", "", $css);
+    }
+  }
+  if ($bbll_options['column_images'] && preg_match_all('/\.fl-node-(.*?)\ >\ (?:.fl-col-content)\ {[ \n]?[ \t]?background-image:[ ]?url\([\'"]?(.*?)\)/', $css, $matches)) {
     for($i=0;$i<count($matches[2]);$i++){
       $css = str_replace("\n\tbackground-image: url(".$matches[2][$i].");\n", "", $css);
     }
@@ -126,10 +238,10 @@ echo '<script>document.addEventListener("DOMContentLoaded", function() {
 function bbll_custom_styles(){
   echo '
   <style>
-  [data-bb-lazy-load-bgurl] .fl-row-content-wrap, [data-bb-lazy-load-bgurl] .fl-col-content{
+  div[data-bb-lazy-load-bgurl].fl-row .fl-row-content-wrap, div[data-bb-lazy-load-bgurl].fl-col .fl-col-content{
     background-image: url(data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==);
   }
-  [data-bb-lazy-load-bgurl] .fl-row-content-wrap, [data-bb-lazy-load-bgurl] .fl-col-content{
+  div[data-bb-lazy-load-bgurl].fl-row .fl-row-content-wrap, div[data-bb-lazy-load-bgurl].fl-col .fl-col-content{
     -webkit-transition: background-image 0.3s;
     transition: background-image 0.3s;
   }
