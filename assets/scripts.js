4597a9eee9864b7870e11125e25dc71fd324e14d
Clay Harmon <clay@clayharmon.me>
2020-11-22 22:13:38 +0300

    Lazy loads ALL beaver builder bg images.

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,1 +1,11 @@
-var lazyLoadInstance = new LazyLoad({ elements_selector: ".bbll" });
+window.addEventListener('load', function() {
+
+  var bbll_bg_store = bbll_bg_obj;
+  for(var i=0; i<bbll_bg_store.length; i++){
+    var cssSelector = bbll_bg_store[i]['selector'];
+    var bg_image = bbll_bg_store[i]['image'];
+    jQuery(cssSelector).addClass('bbll').attr('data-bg', 'url('+bg_image+')');
+  }
+  var lazyLoadInstance = new LazyLoad({ elements_selector: ".bbll" });
+
+});
