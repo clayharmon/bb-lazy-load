09242803d09d03b7ea9bdb30a6887a9fa8d8a8b7
Clay Harmon <clay@clayharmon.me>
2020-12-05 19:15:01 +0300

    add media matching.

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,7 +1,30 @@
-var bbll_bg_store = bbll_bg_obj;
-for(var i=0; i<bbll_bg_store.length; i++){
-  var cssSelector = bbll_bg_store[i]['selector'];
-  var bg_image = bbll_bg_store[i]['image'];
-  jQuery(cssSelector).addClass('bbll').attr('data-bg', 'url('+bg_image+')');
+const bbll_bg_store = bbll_bg_obj;
+const lazyLoadInstance = new LazyLoad({ elements_selector: ".bbll" });
+
+const bbll_selectors = Object.keys(bbll_bg_store.data);
+const bbll_medias = bbll_bg_store.medias;
+
+const bbll_matchedMedia = () => {
+  bbll_selectors.forEach(selector => {
+    let items = bbll_bg_store.data[selector];
+
+    let image;
+    items.forEach(item => {
+      let mq = window.matchMedia(item.media);
+      if(mq.matches) image = item.image;
+    });
+
+    const $el = jQuery(selector);
+    $el.addClass('bbll').attr('data-bg', image);
+    LazyLoad.resetStatus($el[0]);
+  });
+
+  lazyLoadInstance.update();
 }
-var lazyLoadInstance = new LazyLoad({ elements_selector: ".bbll" });
+
+bbll_matchedMedia();
+
+bbll_medias.forEach(media => {
+  const mq = window.matchMedia(media);
+  mq.addEventListener('change', bbll_matchedMedia);
+});
